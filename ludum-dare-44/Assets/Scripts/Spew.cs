6d8968fb739f760a6523f28b871781ed21162563
michael <mikeyjsalmon@gmail.com>
2019-04-26 23:46:51 -0500

    Water 2D

---
+++ ludum-dare-44/Assets/Scripts/Spew.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Water2D;
+
+public class Spew : MonoBehaviour
+{
+    public KeyCode SpaceKey = KeyCode.Space;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(SpaceKey))
+        {
+            transform.localScale -= new Vector3(0.01F, 0.01F, 0);
+            Water2D_Spawner.instance.Dynamic = true;
+            Water2D_Spawner.instance.Spawn(1, new Vector3(10F, 0, 0));
+            Water2D_Spawner.instance.LifeTime = -1;
+        }
+    }
+}
