253ed2534e08ce5d2638895df4ed370bec65d8c2
Brandon Salmon <brandon.salmon@myhealthchampion.com>
2019-04-26 22:12:26 -0500

    Movement script

---
+++ ludum-dare-44/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public KeyCode LeftKey = KeyCode.A;
+    public KeyCode RightKey = KeyCode.D;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var rigidBody = GetComponent<Rigidbody2D>();
+
+        if (Input.GetKey(LeftKey))
+        {
+            rigidBody.AddForce(Vector2.left * 10);
+        }
+        else if (Input.GetKey(RightKey))
+        {
+            rigidBody.AddForce(Vector2.right * 10);
+        }
+    }
+}
